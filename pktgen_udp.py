c5275228450e392475b138a60005f30ce032a2f9
Aravind Dhanasekaran <hdnivara@gmail.com>
2017-08-15 07:31:35 -0700

    udp: bad csum for IPv4 and/or UDP packets

--- pktgen_udp.py
+++ pktgen_udp.py
@@ -9,7 +9,10 @@ import random
 from scapy.all import IP, UDP, Raw
 from scapy.all import get_if_list, get_if_addr, send
 
-BAD_CSUM = 0xDEED
+BAD_CSUM_IP = 0xABCD
+BAD_CSUM_UDP = 0xDEED
+PKT_TYPES = ["ipv4", "udp", "all"]
+
 
 def __parse_args():
     parser = argparse.ArgumentParser(description="Send few UDP packets")
@@ -28,8 +31,8 @@ def __parse_args():
                         default=64, help="size of UDP payload in bytes")
     parser.add_argument("-w", dest="wait", action="store_true",
                         help="wait for user input between packets")
-    parser.add_argument("-b", dest="bad_csum", action="store_true",
-                       help="force bad checksum for UDP packets")
+    parser.add_argument("-b", metavar="PKT-TYPE", dest="bad_csum", type=str,
+                        help="force bad checksum for IPv4 and/or UDP packets")
 
     args = parser.parse_args()
 
@@ -37,31 +40,47 @@ def __parse_args():
         err = "Interface '" + args.interface + "' not found."
         raise parser.error(err)
 
+    if args.bad_csum is not None and args.bad_csum not in PKT_TYPES:
+        err = "Invalid value for '-b PKT-TYPE'. Use one of '%s'." % \
+                ", ".join(PKT_TYPES)
+        raise parser.error(err)
+
     return args
 
 
-def udp_send(sip, dip, dport, size, npackets, bad_csum, wait, interval):
+def udp_send(pkt_cfg):
     """ Send UDP packets. """
-    stat = "sip %s, dip %s, dport %s, size %u" % (sip, dip, dport, size)
+    sip = pkt_cfg['sip']
+    dip = pkt_cfg['dip']
+    dport = pkt_cfg['dport']
+    size = pkt_cfg['size']
+    npackets = pkt_cfg['npackets']
+    bad_csum = pkt_cfg['bad_csum']
+    wait = pkt_cfg['wait']
+    interval = pkt_cfg['interval']
+
+    # Build the packet.
+    if bad_csum in ["ipv4", "all"]:
+        ip = IP(src=sip, dst=dip, chksum=BAD_CSUM_IP)
+    else:
+        ip = IP(src=sip, dst=dip)
+
+    sport = random.randint(4096, 8192)
+    if bad_csum in ["udp", "all"]:
+        udp = UDP(sport=sport, dport=dport, chksum=BAD_CSUM_UDP)
+    else:
+        udp = UDP(sport=sport, dport=dport)
 
     data_tmp = "77 " * size
     data_tmp = data_tmp[:-1].split(" ")
     data = ''.join(data_tmp).decode('hex')
-    
-    ip = IP(src=sip, dst=dip)
-    
-    sport = random.randint(4096, 8192)
-    if bad_csum is True:
-        udp = UDP(sport=sport, dport=dport, chksum=BAD_CSUM)
-    else:
-        udp = UDP(sport=sport, dport=dport)
-        
     payload = Raw(load=data)
 
     pkt = ip/udp/payload
-    stat = "{sip:%s dip:%s}, {sport:%s dport:%s} {csum:%s size:%u}" % \
-            (sip, dip, sport, dport, bad_csum, size)
+    stat = "{sip:%s dip:%s}, {sport:%s dport:%s} {bad-csum:%s size:%u}" % \
+           (sip, dip, sport, dport, bad_csum, size)
 
+    # Send traffic.
     if wait:
         while npackets:
             _ = raw_input("Hit 'enter' to send packet...")
@@ -75,12 +94,11 @@ def udp_send(sip, dip, dport, size, npackets, bad_csum, wait, interval):
 
 def main():
     """ Entry point to the program. """
-    args = __parse_args()
-    print args
+    pkt_cfg = vars(__parse_args())
+    pkt_cfg['sip'] = get_if_addr(pkt_cfg['interface'])
+    print pkt_cfg
 
-    sip = get_if_addr(args.interface)
-    udp_send(sip, args.dip, args.dport, args.size, args.npackets,
-             args.bad_csum, args.wait, args.interval)
+    udp_send(pkt_cfg)
 
 if __name__ == "__main__":
     main()
