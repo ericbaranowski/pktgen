4d23521249229e22a7ded870f7876fe3a0a5bf85
Aravind Dhanasekaran <hdnivara@gmail.com>
2017-08-15 23:26:07 -0700

    tcp: bad csum for IPv4 and/or TCP packets

--- pktgen_tcp.py
+++ pktgen_tcp.py
@@ -9,8 +9,9 @@ import random
 from scapy.all import IP, TCP, Raw
 from scapy.all import get_if_list, get_if_addr, send, sr1
 
-BAD_CSUM = 0xDEED
-DPORT = 5151
+BAD_CSUM_IP = 0xABCD
+BAD_CSUM_TCP = 0xDEED
+PKT_TYPES = ["ipv4", "tcp", "all"]
 
 
 def __parse_args():
@@ -31,14 +32,19 @@ def __parse_args():
     parser.add_argument("-w", dest="wait", action="store_true",
                         help="wait for user input between packets")
     parser.add_argument("-a", dest="synack", action="store_true",
-                        help="use TCP 3-way handshake")
-    parser.add_argument("-b", dest="bad_csum", action="store_true",
-                        help="force bad checksum for TCP packets")
+                        help="use 3-way TCP handshake")
+    parser.add_argument("-b", metavar="PKT-TYPE", dest="bad_csum", type=str,
+                        help="force bad checksum for IPv4 and/or TCP packets")
 
     args = parser.parse_args()
 
     if args.interface not in get_if_list():
         err = "Interface '" + args.interface + "' not found."
+        raise parser.error(err)
+
+    if args.bad_csum is not None and args.bad_csum not in PKT_TYPES:
+        err = "Invalid PKT-TYPE for '-b'. Use one of '%s'." % \
+                ", ".join(PKT_TYPES)
         raise parser.error(err)
 
     return args
@@ -56,36 +62,41 @@ def tcp_send(pkt_cfg):
     wait = pkt_cfg['wait']
     interval = pkt_cfg['interval']
 
+    # Build the packet.
+    if bad_csum in ["ipv4", "all"]:
+        ip = IP(src=sip, dst=dip, chksum=BAD_CSUM_IP)
+    else:
+        ip = IP(src=sip, dst=dip)
+
+    sport = random.randint(4096, 8192)
+    if bad_csum in ["tcp", "all"]:
+        tcp = TCP(sport=sport, dport=dport, chksum=BAD_CSUM_TCP)
+    else:
+        tcp = TCP(sport=sport, dport=dport)
+
     data_tmp = "77 " * size
     data_tmp = data_tmp[:-1].split(" ")
     data = ''.join(data_tmp).decode('hex')
-
-    # Build the packet.
-    ip = IP(src=sip, dst=dip)
-    sport = random.randint(4096, 8192)
-    if bad_csum is True:
-        tcp = TCP(sport=sport, dport=dport, chksum=BAD_CSUM)
-    else:
-        tcp = TCP(sport=sport, dport=dport)
     payload = Raw(load=data)
-    pkt = ip/tcp/payload
 
-    stat = "{sip:%s dip:%s}, {sport:%s dport:%s} {csum:%s size:%u}" % \
+    pkt = ip/tcp/payload
+    stat = "{sip:%s dip:%s}, {sport:%s dport:%s} {bad-csum:%s size:%u}" % \
            (sip, dip, sport, dport, bad_csum, size)
 
+    # 3-way TCP handshake to setup connection.
     if tway is True:
-        # Send TCP SYN packet and receive TCP SYN-ACK packet.
+        good_ip = IP(src=sip, dst=dip)
+
         seq_seqno = 5000
         tcp_syn = TCP(sport=sport, dport=dport, flags='S', seq=seq_seqno)
-        tcp_synack = sr1(ip/tcp_syn)
+        tcp_synack = sr1(good_ip/tcp_syn)
         print "Sent TCP SYN with seq %u" % (seq_seqno)
 
-        # Send TCP ACK packet.
         ack_seqno = tcp_synack.ack
         ack_ackno = tcp_synack.seq + 1
         tcp_ack = TCP(sport=sport, dport=dport, flags='A',
                       seq=ack_seqno, ack=ack_ackno)
-        send(ip/tcp_ack)
+        send(good_ip/tcp_ack)
         print "Sent TCP ACK with seq %u, ack %u" % (ack_seqno, ack_ackno)
 
     # Send traffic.
